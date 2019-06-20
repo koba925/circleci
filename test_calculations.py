3372e9fbf46e1d9f2efde90494bd8293a651b877
Takahiro Kobayashi <koba925@gmail.com>
2019-06-20 21:24:57 +0900

    Initial commit

---
+++ test_calculations.py
@@ -0,0 +1,16 @@
+import unittest
+import calculations
+
+
+class CalculationsTest(unittest.TestCase):
+    """割り算する関数をテストする"""
+
+    def test_divide_normal(self):
+        self.assertEqual(calculations.divide(2, 2), 1)
+
+    def test_divide_contain_zero(self):
+        self.assertEqual(calculations.divide(0, 1), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
