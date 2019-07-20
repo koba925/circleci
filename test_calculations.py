dc03386dfc92890d348c66939c4e7cafbef0eba8
Takahiro Kobayashi <koba925@gmail.com>
2019-07-20 10:50:53 +0900

    back to normal

--- test_calculations.py
+++ test_calculations.py
@@ -8,7 +8,7 @@ class CalculationsTest(unittest.TestCase):
         self.assertEqual(calculations.divide(2, 2), 1)
 
     def test_divide_contain_zero(self):
-        self.assertEqual(calculations.divide(0, 1), 1)
+        self.assertEqual(calculations.divide(0, 1), 0)
 
 
 if __name__ == "__main__":
